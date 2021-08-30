3a44cf530b0719c47ac39148d367a97f3c51a02d
AndersonAndrad <31743641+AndersonAndrad@users.noreply.github.com>
2021-08-30 18:53:11 -0300

    missing fix - correct the hooks permissions

---
+++ src/hooks/useCan.hook.ts
@@ -0,0 +1,34 @@
+import { useAuth } from "../context/Auth.context"
+
+type UserCanParams = {
+  permissions?: string[]
+  roles?: string[]
+}
+
+export function useCan ( { permissions = [], roles = [] }: UserCanParams ) {
+  const { user, isAuthenticated } = useAuth()
+
+  if ( !isAuthenticated ) {
+    return false
+  }
+
+  if ( permissions?.length > 0 ) {
+    const hasAllPermissions = permissions.every( permission => { return user.permissions.includes( permission ) } )
+
+    if ( !hasAllPermissions ) {
+      return false
+    }
+
+  }
+
+  if ( roles?.length > 0 ) {
+    const hasAllRoles = roles.some( role => { return user.roles.includes( role ) } )
+
+    if ( !hasAllRoles ) {
+      return false
+    }
+  }
+
+  return true
+
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,21 +1,27 @@
 import { GetServerSidePropsContext } from "next"
 import { useAuth } from "../context/Auth.context"
+import { useCan } from "../hooks/useCan.hook"
 import { setupAPIClient } from "../services/api.services"
 import { withSSRAuth } from "../utils/withSSRAuth.utils"
 
 export default function Dashboard () {
   const { user } = useAuth()
 
+  const useCanSeeMetrics = useCan( {
+    permissions: ["metrics.list"],
+  } )
+
   return (
     <div>
       <h1>{user?.email}</h1>
+      {useCanSeeMetrics && <div>metrics</div>}
     </div>
   )
 }
 
-export const getServerSideProps = withSSRAuth( ( context: GetServerSidePropsContext ) => {
+export const getServerSideProps = withSSRAuth( async ( context: GetServerSidePropsContext ) => {
   const apiClient = setupAPIClient( context )
-
+  const response = await apiClient.get( '/me' )
 
   return {
     props: {}
